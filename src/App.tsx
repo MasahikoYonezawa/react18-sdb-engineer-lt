793e54b36c1035cdb367a188ba46784b5af47579
MasahikoYonezawa <y.masahiko0802@gmail.com>
2023-04-06 23:26:58 +0900

    feat: suspenceの基本

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 import { AutoBatchEventHandler } from "./components/AutoBatchEventHandler";
 import { AutoBatchOther } from "./components/AutoBatchOther";
 import { Transition } from "./components/Transition";
+import { ReactQuery } from "./components/ReactQuery";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   console.log("Appがレンダリング！");
@@ -12,6 +14,12 @@ function App() {
       <AutoBatchOther />
       <hr />
       <Transition />
+      <hr />
+      <ErrorBoundary fallback={<h1>全体エラーだよ〜</h1>}>
+        <Suspense fallback={<p>全体ローディング中だよ</p>}>
+          <ReactQuery />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
